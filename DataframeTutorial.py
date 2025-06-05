fd31973c3a58c12ab7043dbce7c97466c2a6ed4c
crystal00lim <71657775+crystal00lim@users.noreply.github.com>
2025-06-05 17:03:35 -0400

    Update DataframeTutorial.py

--- DataframeTutorial.py
+++ DataframeTutorial.py
@@ -5,3 +5,14 @@
 # "pip install pandas"
 import pandas as pd
 
+# First, create lists representing the columns of each dataframe.
+names = ['Ketzia', 'Joey', 'Sara', 'Steve', 'Silvia', 'JongHo', 'Nelson', 'Momo', 'Nancy']
+occupation = ['Dancer', 'Assistant Designer', 'Program associate', 'programmer', 'Architect', 'Architect', 'Freelancer',
+              'Unemployed', 'Assistant Designer']
+age = [25, 24, 25, 27, 24, 26, 24, 24, 24]
+company = ['none', 'BCBGMAXAZRIAGROUP', 'The Korea Society', 'Samsung', 'GHS Architects', 'GHS Architects', 'none',
+           'none', 'Calvin Klein']
+
+# Use len() to figure out how many items are on the list
+# You want to make sure all the lists have the same amount of items
+print(len(names))
