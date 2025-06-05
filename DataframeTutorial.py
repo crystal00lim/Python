8593124f2160a0750e41037f2f644f33a597d8cb
crystal00lim <71657775+crystal00lim@users.noreply.github.com>
2025-06-05 17:03:17 -0400

    Create DataframeTutorial.py

---
+++ DataframeTutorial.py
@@ -0,0 +1,7 @@
+# This code will explain how to create a dataframe
+
+# First, import pandas, it is a package that will set the dataframe into table form.
+# You can download the package by going into the console, and entering:
+# "pip install pandas"
+import pandas as pd
+
